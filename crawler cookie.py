22a4f6c7b2a181cb31e443d3a9854330b7b63c22
B09902005 <b09902005@ntu.edu.tw>
2021-09-09 17:14:58 +0800

    new course

--- crawler cookie.py
+++ crawler cookie.py
@@ -3,6 +3,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 import urllib.request as req
 
+import urllib
+
 import bs4
 
 # This a crawler for ptt NTUcourse
@@ -17,15 +19,21 @@ def printcontext(url):
     print()
     return
 
-def printtitle(url):
-    context = makecontext(url)
-    titles = context.find_all("div", class_="title")
-    for title in titles:
-        if (title.a != None) and (title.a.string.find('通識') == -1):
-            #url = "https://www.ptt.cc" + title.a['href']
-            #printcontext(url)
-            print(title.a.string)
-    return
+def findarticle(url):
+    num = 0
+    href = []
+    while (True):
+        context = makecontext(url)
+        titles = context.find_all("div", class_="title")
+        for title in titles:
+            if (title.a != None):
+                href.append("https://www.ptt.cc" + title.a["href"])
+                print(num,title.a.string)
+                num += 1
+        url = prevpage(url)
+        if (url == 0):
+            break
+    return href
 
 def prevpage(url):
     context = makecontext(url)
@@ -36,20 +44,16 @@ def prevpage(url):
         return 0
 
 def makecontext(url):
-    request = req.Request(url, headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"})
-    with req.urlopen(request) as response:
+    request = urllib.request.Request(url, headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"})
+    with urllib.request.urlopen(request) as response:
         data = response.read().decode("utf-8")
     context = bs4.BeautifulSoup(data,"html.parser")
     return context
 
 if __name__ == '__main__':
     what = input('輸入你想搜尋的教授或課程名稱。')
-    url = "https://www.ptt.cc/bbs/NTUcourse/search?q=" + urllib.quote(what)
-    #url = "https://www.ptt.cc/bbs/NTUcourse/index.html"
-    #url = "https://www.ptt.cc/bbs/NTUcourse/search?q=%E5%91%82%E5%AD%B8%E4%B8%80"
-    #url = "https://www.ptt.cc/bbs/NTUcourse/search?q=%E6%9E%97%E8%BB%92%E7%94%B0"
-    for i in range (3):
-        printtitle(url)
-        url = prevpage(url)
-        if (url == 0):
-            break
+    url = "https://www.ptt.cc/bbs/NTUcourse/search?q=" + urllib.parse.quote(what)
+    href = findarticle(url)
+    num = int(input('\n請輸入你想進入的文章的編號。'))
+    url = href[num]
+    printcontext(url)
